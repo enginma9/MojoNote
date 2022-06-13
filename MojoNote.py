79f4040da897ac195146ce4b2d26118c71be3884
Joseph Loar <enginma2@gmail.com>
2022-06-12 19:43:26 -0500

    added preferences functionality and sizegrip

--- MojoNote.py
+++ MojoNote.py
@@ -13,21 +13,17 @@ class MainWindow(QMainWindow):
 
     def Set_Up(self):
         #open the file to pull data from
-        location = os.environ['HOME'] + '/Python/MacTest/.Notes/'
+        location = os.environ['HOME'] + '/Python/MacTest/.Notes/' #'/.Library/MojoNote/'
         filename = '0.MojoNote'
         filepath = location + filename
-        #start paste
-        print("File to open: " + filename )
-        print("Open: " +  filepath + "\n ")
         try:
             File_Handle = open( filepath, 'r')
             File_Contents = str( File_Handle.read() )
             tree = ET.fromstring( File_Contents )
             Listing = tree.findall('note')
-            print('Note count:', len( tree.findall('note') ))
         except:
             print("Loading note list failed.")
-
+            quit()
         self.Note_List = list()
         self.Notes = 0
 
@@ -40,19 +36,19 @@ class MainWindow(QMainWindow):
             Pos_Y = int( item.find('Position_y').text)
             BG_Color = str(item.find('BG_Color').text)
             FG_Color = str(item.find('FG_Color').text)
-
-            #print(box_name, "Position:(" + str(Pos_X) + "," + str(Pos_Y) + ") Size:(" + str(width) + "," + str(height) + ")", BG_Color,FG_Color)
-
-            checkbox_Item = QListWidgetItem( box_name )
+            checkbox_Item = QListWidgetItem( (box_id + ".  " + box_name ) )
             checkbox_Item.setFlags( checkbox_Item.flags() | Qt.ItemIsUserCheckable )
             checkbox_Item.setCheckState( Qt.Unchecked )
             self.listWidget.addItem( checkbox_Item )
             Sub_Window = Note_Window()
             self.Sub_Windows.append( Sub_Window )
-            #self.Sub_Windows[self.Notes].show()
+            # May take this and set it to a function of the Note window itself
+            note_filepath = location + box_id + ".MojoNote"
+            Note_Handle = open( note_filepath, 'r')
+            Note_Contents = str( Note_Handle.read() )
+            self.Sub_Windows[self.Notes].Note_Area.setPlainText( Note_Contents )
             self.Sub_Windows[self.Notes].resize(width,height)
             self.Sub_Windows[self.Notes].move(Pos_X, Pos_Y)
-            #self.Sub_Windows[Notes].setStyleSheet(f'QWidget {{background-color: {BG_Color}; font-size: 14pt; color: {FG_Color};}}') #replace with custom class function
             self.Notes = self.Notes + 1
             self.show_hide()
 
@@ -64,6 +60,9 @@ class MainWindow(QMainWindow):
                 self.Sub_Windows[number].hide()
 
     def Open_Preferences_Window(self):
+        # get which item is selected, and pass that number to preferences, then make sure the window is shown
+        selected = self.listWidget.currentRow()
+        self.P_Window.get_info( selected )
         self.P_Window.show()
         return 0
 
@@ -103,7 +102,7 @@ class MainWindow(QMainWindow):
         self.Set_Up()
         # create Preferences window, and hide it.
         self.P_Window = Pref_Window()
-        self.P_Window.show()
+        self.P_Window.hide()
         self.Preferences_Button.clicked.connect( self.Open_Preferences_Window )
         # May have to go back and use QListView, and manually implement checkbox (space to check will not activate this ˅ and there doesn't seem to be a QT signal to attach the command to the checkbox directly when it is a checkable list item)
         self.listWidget.itemClicked.connect(lambda: self.show_hide())
@@ -115,26 +114,44 @@ class Note_Window(QDialog):
 
     def __init__(self):
         super(Note_Window, self).__init__()
-        self.BG_Color = "\#000000"
-        self.FG_Color = "\#ffffff"
+        self.BG_Color = "330099"
+        self.FG_Color = "ffffff"
         self.Title = "Note"
+        self.Opacity = 0.95 # May want to allow changing this transparency from 91-100, but you can also just make the color 2 digits longer for transparency.
         self.Main_Layout  = QVBoxLayout()
         self.Main_Layout.setContentsMargins(0,0,0,0)
         self.setWindowTitle("MacTest")
-        self.setWindowOpacity(0.9)
-        self.Main_Layout.addWidget( MyBar( self ) )
-        self.Note_Area = QPlainTextEdit("Enter text here.")
+        self.setWindowOpacity( self.Opacity )
+        self.Title_Bar = MyBar(self)
+        self.Main_Layout.addWidget( self.Title_Bar )
+        self.Note_Area = QPlainTextEdit()
         self.Main_Layout.addWidget( self.Note_Area )
         self.Note_Area.setStyleSheet('border: none; font: 12px Arial;')
+        self.sizegrip = QSizeGrip(self)
+        self.sizegrip.setFixedSize(20, 10)
+        self.Main_Layout.addWidget(self.sizegrip)
+        self.Main_Layout.setAlignment(self.sizegrip, Qt.AlignBottom | Qt.AlignRight)
         self.setLayout(self.Main_Layout)
         self.Main_Layout.setContentsMargins(0,0,0,0)
         self.Main_Layout.addStretch(-1)
-        self.setMinimumSize(200,100)
+        self.setMinimumSize(100,30)
         self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setStyleSheet(f'QWidget {{background-color: {self.BG_Color}; font-size: 12pt; color: {self.FG_Color};}}')
+        self.setStyleSheet(f'QWidget {{background-color: \#{self.BG_Color}; font-size: 12pt; color: \#{self.FG_Color};}}')
         self.pressing = False
-        self.sizegrip = QSizeGrip(self)
-        self.Main_Layout.addWidget(self.sizegrip, 0, Qt.AlignBottom | Qt.AlignRight)
+
+    def resizeEvent(self, QResizeEvent):
+        super(Note_Window, self).resizeEvent(QResizeEvent)
+        self.Note_Area.setMinimumSize( self.width(), self.height() - 30 )
+
+    def change_Foreground(self, FG):
+        self.FG_Color = FG
+
+    def change_Background(self, BG):
+        self.BG_Color = BG
+
+    def update_formatting(self):
+        self.setStyleSheet(f"QWidget {{background-color: \#{self.BG_Color}; font-size: 10pt; color: \#{self.FG_Color};}}")
+        self.Title_Bar.update_formatting()
 
 class MyBar(QWidget):
 
@@ -147,25 +164,25 @@ class MyBar(QWidget):
 
         btn_size = 10
 
-        self.Menu_Button = QPushButton("X", flat=True)   #("Ξ")
+        self.Menu_Button = QPushButton("Ξ", flat=True)   #("Ξ")
         self.Menu_Button.clicked.connect(self.Menu_Button_clicked)
         self.Menu_Button.setFixedSize(btn_size,btn_size)
-        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
+        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
         self.Restore_btn = QPushButton("o", flat=True)
         self.Restore_btn.clicked.connect(self.Restore_btn_clicked)
         self.Restore_btn.setFixedSize(btn_size, btn_size)
-        self.Restore_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
+        self.Restore_btn.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
         self.Roll_Up_btn = QPushButton("-", flat=True)
-        #self.Roll_Up_btn.clicked.connect(self.change_background)
+        self.Roll_Up_btn.clicked.connect(self.Roll_Up_btn_clicked)
         self.Roll_Up_btn.setFixedSize(btn_size,btn_size)
-        self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
+        self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
         self.Max_Button = QPushButton("O", flat=True)
         self.Max_Button.clicked.connect(self.Max_Button_clicked)
         self.Max_Button.setFixedSize(btn_size, btn_size)
-        self.Max_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
+        self.Max_Button.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
         self.title.setFixedHeight(btn_size)
         self.title.setAlignment(Qt.AlignCenter)
@@ -180,7 +197,7 @@ class MyBar(QWidget):
             font-size: 10pt;
         """)
         #Set to same color as parent
-        self.title.setStyleSheet(f'QWidget {{background-color: {self.parent.BG_Color};}}')
+        self.title.setStyleSheet(f'QWidget {{background-color: \#{self.parent.BG_Color};}}')
         self.setLayout(self.Bar_Layout)
 
         self.start = QPoint(0, 0)
@@ -208,50 +225,42 @@ class MyBar(QWidget):
         self.pressing = False
 
     def Menu_Button_clicked(self):
-        self.parent.hide()
+        window.show()
 
     def Max_Button_clicked(self):
         self.parent.showMaximized()
-        # Not a true "maximized" window, but that is beneficial,
-        #  because when on MacOS, I don't want to take over the screen,
-        #  just take up available space.
 
     def Roll_Up_btn_clicked(self):
-        self.parent.showNormal()
+        #print(self.parent.width())
+        self.parent.resize(self.parent.width(), 30 ) # self.parent.height())
 
     def Restore_btn_clicked(self):
         self.parent.showNormal()
 
     def change_background(self):
-        self.change_Background("\#ffffff")
-        self.change_Foreground("\#000000")
-        self.update_formatting()
-
-    def change_Foreground(self, FG):
-        self.parent.FG_Color = FG
-
-    def change_Background(self, BG):
-        self.parent.BG_Color = BG
+        self.parent.change_Background("\#ffffff")
+        self.parent.change_Foreground("\#000000")
+        self.parent.update_formatting()
 
     def update_formatting(self):
-        self.parent.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.title.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Restore_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Max_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #print( self.parent.BG_Color, self.parent.FG_Color, self.parent.pressing)
+        self.title.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
+        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
+        self.Restore_btn.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
+        self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
+        self.Max_Button.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
 class Pref_Window(QDialog):
 
     def __init__(self):
         super().__init__()
-        self.BG_Color = "\#000000"
-        self.FG_Color = "\#ffffff"
+        self.BG_Color = "110044"
+        self.FG_Color = "ffffff"
         self.Title = "Preferences"
+        self.selected = 1
         self.Main_Layout  = QVBoxLayout()
         self.Main_Layout.setContentsMargins(0,0,0,0)
-        self.setWindowTitle("MacTest")
+        #self.setWindowTitle("MacTest")
+        self.setStyleSheet('border-radius: 15px;') #
         self.setWindowOpacity(0.9)
         self.Main_Layout.addWidget( Pref_Bar( self ) )
 
@@ -294,21 +303,51 @@ class Pref_Window(QDialog):
         self.Button_Layout = QHBoxLayout()
         self.Refresh_Button = QPushButton("Refresh")
         self.Button_Layout.addWidget( self.Refresh_Button )
-        #self.Refresh_Button.clicked.connect(self.Refresh_Button_clicked)
+        #self.Refresh_Button.clicked.connect(self.get_info(self.selected))
         self.Set_Button = QPushButton("Set")
         self.Button_Layout.addWidget( self.Set_Button )
-        #self.Set_Button.clicked.connect(self.Set_Button_clicked)
+        self.Set_Button.clicked.connect(self.Set_Button_clicked)
         self.Main_Layout.addLayout( self.Button_Layout )
 
-
-        #self.Note_Area.setStyleSheet('border: none; font: 12px Arial;')
         self.setLayout(self.Main_Layout)
         self.Main_Layout.setContentsMargins(0,0,0,0)
         self.Main_Layout.addStretch(-1)
         self.setMinimumSize(200,100)
         self.setWindowFlags(Qt.FramelessWindowHint)
-        self.setStyleSheet(f'QWidget {{background-color: {self.BG_Color}; font-size: 12pt; color: {self.FG_Color};}}')
+        self.setStyleSheet(f'QWidget {{background-color: \#{self.BG_Color}; font-size: 12pt; color: \#{self.FG_Color};}}')
         self.pressing = False
+
+    def get_info( self, selected=1 ):
+        self.selected = selected
+        x = window.Sub_Windows[selected].Title
+        self.Title_Edit.setText(x)
+        x = window.Sub_Windows[selected].frameGeometry().x()
+        self.Position_x_Edit.setText(str(x))
+        x = window.Sub_Windows[selected].frameGeometry().y()
+        self.Position_y_Edit.setText(str(x))
+        x = window.Sub_Windows[selected].frameGeometry().width()
+        self.Size_x_Edit.setText(str(x))
+        x = window.Sub_Windows[selected].frameGeometry().height()
+        self.Size_y_Edit.setText(str(x))
+        x = window.Sub_Windows[selected].BG_Color
+        self.BG_Color_Edit.setText(x)
+        x = window.Sub_Windows[selected].FG_Color
+        self.FG_Color_Edit.setText(x)
+        x = window.Sub_Windows[selected].Title
+
+    def Set_Button_clicked(self):
+        x = int(self.Size_x_Edit.text())
+        y = int(self.Size_y_Edit.text())
+        window.Sub_Windows[self.selected].resize( x, y )
+        x = int(self.Position_x_Edit.text())
+        y = int(self.Position_y_Edit.text())
+        window.Sub_Windows[self.selected].move( x, y )
+        #if this field has only 6x 0-9/a-f characters, else: turn label red
+        window.Sub_Windows[self.selected].change_Background( self.BG_Color_Edit.text() )
+        #if this field has only 6x 0-9 characters, else: turn label red
+        window.Sub_Windows[self.selected].change_Foreground( self.FG_Color_Edit.text() )
+        window.Sub_Windows[self.selected].update_formatting()
+        return 0
 
 class Pref_Bar(QWidget):
 
@@ -324,22 +363,7 @@ class Pref_Bar(QWidget):
         self.Menu_Button = QPushButton("X", flat=True)   #("Ξ")
         self.Menu_Button.clicked.connect(self.Menu_Button_clicked)
         self.Menu_Button.setFixedSize(btn_size,btn_size)
-        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-
-        self.Restore_btn = QPushButton("o", flat=True)
-        self.Restore_btn.clicked.connect(self.Restore_btn_clicked)
-        self.Restore_btn.setFixedSize(btn_size, btn_size)
-        self.Restore_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-
-        self.Roll_Up_btn = QPushButton("-", flat=True)
-        self.Roll_Up_btn.clicked.connect(self.change_background)
-        self.Roll_Up_btn.setFixedSize(btn_size,btn_size)
-        self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-
-        self.Max_Button = QPushButton("O", flat=True)
-        self.Max_Button.clicked.connect(self.Max_Button_clicked)
-        self.Max_Button.setFixedSize(btn_size, btn_size)
-        self.Max_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
+        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: \#{self.parent.BG_Color}; font-size: 10pt; color: \#{self.parent.FG_Color};}}")
 
         self.title.setFixedHeight(btn_size)
         self.title.setAlignment(Qt.AlignCenter)
@@ -351,7 +375,7 @@ class Pref_Bar(QWidget):
             font-size: 10pt;
         """)
         #Set to same color as parent
-        self.title.setStyleSheet(f'QWidget {{background-color: {self.parent.BG_Color};}}')
+        self.title.setStyleSheet(f'QWidget {{background-color: \#{self.parent.BG_Color};}}')
         self.setLayout(self.Bar_Layout)
 
         self.start = QPoint(0, 0)
@@ -383,39 +407,6 @@ class Pref_Bar(QWidget):
 
     def Max_Button_clicked(self):
         self.parent.showMaximized()
-        # Not a true "maximized" window, but that is beneficial,
-        #  because when on MacOS, I don't want to take over the screen,
-        #  just take up available space.
-
-    def Roll_Up_btn_clicked(self):
-        self.parent.showNormal()
-
-    def Restore_btn_clicked(self):
-        self.parent.showNormal()
-
-    def change_background(self):
-        self.change_Background("\#ffffff")
-        self.change_Foreground("\#000000")
-
-    def change_Foreground(self, FG):
-        self.parent.FG_Color = FG
-        self.parent.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 14pt; color: {self.parent.FG_Color};}}")
-        self.title.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Restore_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Max_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-
-    def change_Background(self, BG):
-        self.parent.BG_Color = BG
-        self.parent.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.title.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        self.Menu_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Restore_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Roll_Up_btn.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #self.Max_Button.setStyleSheet(f"QWidget {{background-color: {self.parent.BG_Color}; font-size: 10pt; color: {self.parent.FG_Color};}}")
-        #print( self.parent.BG_Color, self.parent.FG_Color, self.parent.pressing)
-
 
 app = QApplication(sys.argv)
 window = MainWindow()
